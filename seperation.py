d4017d0adc6dd20a001cce002027d7ffc971dcd3
KNRK RAJU ALLURI <knrkrajualluri@gmail.com>
2016-10-17 10:36:04 +0530

    Update seperation.py

--- seperation.py
+++ seperation.py
@@ -10,18 +10,18 @@ with open(sys.argv[1]) as fin:
 	
     for line in fin:
         parts = line.split() # split line into parts
-        sid=parts[0]
-	fid=parts[1]
-	cg=parts[2]
+        sid=parts[0]  # speaker id
+	fid=parts[1]  # file id
+	cg=parts[2]   # category ie human/spoof
 	if not os.path.exists(str(sid)):
 			os.makedirs(str(sid))
 	if cg == "human":
-		src="/media/knrk/MyWork/NCC&SPIN/ASV-Spoof/wav/"+sid+"/"+fid+".wav"
-		dst="/media/knrk/MyWork/NCC&SPIN/ASV-Spoof/newdatabase/"+sid+"/"+fid+".wav"
+		src="/media/knrk/MyWork/NCC&SPIN/ASV-Spoof/wav/"+sid+"/"+fid+".wav"  # source path
+		dst="/media/knrk/MyWork/NCC&SPIN/ASV-Spoof/newdatabase/"+sid+"/"+fid+".wav" # destinaton path
 		#print src
 		#print dst
 		print cg
-		copyfile(src, dst)
+		copyfile(src, dst)  # to copy files
 				
 		
 	
